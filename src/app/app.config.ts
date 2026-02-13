62a2b2331d19f427db780ac9a73d4c06839549b6
Hugo <hugopalominogalindo974@gmail.com>
2026-02-13 12:41:41 -0500

    Agregar PrimeNG y PrimeIcons, actualizar configuraciones y mejorar la estructura del proyecto

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,16 +5,20 @@ import { providePrimeNG } from 'primeng/config';
 
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { routes } from './app.routes';
+// import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    // provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
       },
     }),
+    // provider para HTTP Client usando Fetch API, que es más moderna y eficiente que XMLHttpRequest
+    // provideHttpClient(withFetch()),
   ],
 };
