04a5830ce65f3e5aced3d61e6df96e67cdb2b8cc
Hugo <hugopalominogalindo974@gmail.com>
2026-02-19 15:36:31 -0500

    feat: restaurar la exportación del manejador de solicitudes a su forma asíncrona

--- api/index.js
+++ api/index.js
@@ -1,11 +1,4 @@
-// export default async (req, res) => {
-//   const { reqHandler } = await import('../dist/angular-SSR-vercel/server/server.mjs');
-//   return reqHandler(req, res);
-// };
-
-// api/index.js
-const { reqHandler } = require('../dist/<your-project-name>/server/server.mjs');
-
-module.exports = (req, res) => {
+export default async (req, res) => {
+  const { reqHandler } = await import('../dist/control-de-procesos/server/server.mjs');
   return reqHandler(req, res);
 };
