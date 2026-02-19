49983bfcb2b24df7d639257ff0e24dbf0d76b83b
Hugo <hugopalominogalindo974@gmail.com>
2026-02-19 15:32:55 -0500

    feat: actualizar la exportación del manejador de solicitudes a un módulo común

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
-export default async (req, res) => {
-  const { reqHandler } = await import('../dist/angular-SSR-vercel/server/server.mjs');
+// export default async (req, res) => {
+//   const { reqHandler } = await import('../dist/angular-SSR-vercel/server/server.mjs');
+//   return reqHandler(req, res);
+// };
+
+// api/index.js
+const { reqHandler } = require('../dist/<your-project-name>/server/server.mjs');
+
+module.exports = (req, res) => {
   return reqHandler(req, res);
 };
