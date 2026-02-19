d00715080292dd023f9e66d2a484908099f188d7
Hugo <hugopalominogalindo974@gmail.com>
2026-02-19 14:22:15 -0500

    feat: actualizar el modo de renderizado para rutas inexistentes y establecer el estado 404 en el servidor

--- src/app/features/not-found/not-found.ts
+++ src/app/features/not-found/not-found.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, Optional, RESPONSE_INIT } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './not-found.css',
 })
 export class NotFound {
-
+  constructor(@Optional() @Inject(RESPONSE_INIT) private response: any) {
+    if (this.response) {
+      this.response.status(404); // Establece el status 404 real en el servidor
+    }
+  }
 }
