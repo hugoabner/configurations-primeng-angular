c9e2afbc9bdad44237e1951570cb3468a07a47e0
Hugo <hugopalominogalindo974@gmail.com>
2026-02-19 13:01:04 -0500

    feat: mejorar la estructura de rutas en app.routes.ts y ajustar la carga de componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,31 +2,31 @@ import { Routes } from '@angular/router';
 import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
 
 export const routes: Routes = [
+  // si la ruta es vacía, redirige a 'polizas' dentro del DashboardLayout
+  { path: '', pathMatch: 'full', redirectTo: 'polizas' },
   {
     path: '',
     component: DashboardLayout,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: 'polizas' },
       {
-
         // si es full: la ruta debe coincidir exactamente con 'polizas' para cargar el componente Poliza.
         // si es prefix: cualquier ruta que comience con 'polizas' (como 'polizas/details') también cargaría el componente Poliza, lo cual podría no ser deseado.
-        pathMatch: 'full', // | prefix
+        // pathMatch: 'full', // | prefix
         title: 'Pólizas | Karlos Seguros',
         path: 'polizas',
-        loadComponent: () => import('./features/process-features/import-poliza-process-feature/poliza/poliza').then((m) => m.Poliza),
+        loadComponent: () =>
+          import('./features/process-features/import-poliza-process-feature/poliza/poliza').then(
+            (m) => m.Poliza,
+          ),
       },
       // Opción A: 404 dentro del Layout
       {
         title: 'Página no encontrada | Karlos Seguros',
         path: 'not-found',
-        loadComponent: () => import('./features/not-found/not-found').then(m => m.NotFound)
+        loadComponent: () => import('./features/not-found/not-found').then((m) => m.NotFound),
       },
     ],
   },
-  // Opción B: 404 fuera del Layout (Pantalla completa)
-  {
-    path: '**',
-    redirectTo: 'not-found' // O carga directamente el componente aquí
-  }
+  // si la ruta no coincide con ninguna de las anteriores, redirige a 'not-found' para mostrar la página de error
+  { path: '**', redirectTo: 'not-found' },
 ];
